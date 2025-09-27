78f6623acbe40089a15c1316cbfe9ebddef5413a
LSD_Villax <andresvillabon@gmail.com>
2025-09-27 17:01:48 -0500

    estructure

--- src/database/database.js
+++ src/database/database.js
@@ -3,5 +3,5 @@ import {Sequelize} from "sequelize";
 export const sequelize = new Sequelize("nocta", "postgres", "102505", {
   port: "5432",
   host: "localhost",
-  dialect: "postgres"
+  dialect: "postgres",
 });
